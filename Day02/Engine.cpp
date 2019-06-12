594f9357f468814d4daf4430560ed70a747cff18
Junios <junios@junios.net>
2019-06-12 15:32:10 +0900

    class화 해서 만들기 완성

--- Day02/Engine.cpp
+++ Day02/Engine.cpp
@@ -1,8 +1,16 @@
 #include "Engine.h"
+#include "Player.h"
+#include "Map.h"
 #include <conio.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+
+void Engine::Init(Player * _player, Map * _map)
+{
+	map = _map;
+	map->Init(_player);
+}
 
 void Engine::Run()
 {
@@ -17,47 +25,20 @@ void Engine::Run()
 void Engine::Draw()
 {
 	system("cls");
-	//for (int Y = 0; Y < 10; ++Y)
-	//{
-	//	for (int X = 0; X < 10; ++X)
-	//	{
-	//		if (X == PlayerX && Y == PlayerY)
-	//		{
-	//			printf("@ ");
-	//		}
-	//		else
-	//		{
-	//			printf("%d ", Map[Y][X]);
-	//		}
-	//	}
-	//	printf("\n");
-	//}
+	map->Draw();
 }
 
 void Engine::Tick(int KeyCode)
 {
-	//switch (KeyCode)
-	//{
-	//case RIGHT:
-	//	PlayerX++;
-	//	break;
-	//case LEFT:
-	//	PlayerX--;
-	//	break;
-	//case UP:
-	//	PlayerY--;
-	//	break;
-	//case DOWN:
-	//	PlayerY++;
-	//	break;
-	//case 'Q':
-	//case 'q':
-	//	bIsRunning = false;
-	//	break;
-	//}
+	switch (KeyCode)
+	{
+	case 'Q':
+	case 'q':
+		bIsRunning = false;
+		break;
+	}
 
-	//PlayerX = Clamp(PlayerX, 1, 8);
-	//PlayerY = Clamp(PlayerY, 1, 8);
+	map->Tick(KeyCode);
 }
 
 int Engine::Input()
